17705d89ecfe49aec40d1a58896f8766a276b388
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-22 17:39:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -148,13 +148,9 @@ end
 def name_lookup(number)
   game_hash.each do |home_away, team_info|
     binding.pry
-    team_info[:players].each do |info, data|
-      if info == :players
-        data.each do |player|
-          if player[:number] == number
-            return player[:player_name]
-          end
-        end
+    team_info[:players].each do |player|
+      if player[:number] == number
+        return player[:player_name]
       end
     end
   end
